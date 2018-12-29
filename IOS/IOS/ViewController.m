dae754a21a179d9286852f6ebb548c98ac9ea345
杨丰林 <1490719714@qq.com>
2018-12-29 09:49:17 +0800

    initVIew

--- IOS/IOS/ViewController.m
+++ IOS/IOS/ViewController.m
@@ -16,13 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.title = @"IOS";
+    self.view.backgroundColor = [UIColor yellowColor];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
