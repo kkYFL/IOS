b8845ed7dded97bb5cde589811bdead04ac4099a
杨丰林 <1490719714@qq.com>
2019-01-03 20:15:07 +0800

    ss

--- IOS/Base/Headers/YFLBase.h
+++ IOS/Base/Headers/YFLBase.h
@@ -37,5 +37,114 @@
 #define HEIGHT_SCALE [UIScreen mainScreen].bounds.size.height/667.0
 
 
+// 导航栏右按钮(只有文字)
+#define NAVIGATION_BAR_RIGHT_BUTTON_ONLYTEXT(x, y, w, h, normal, selected, sel)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setTitle:normal forState:UIControlStateNormal];\
+[button setTitle:selected forState:UIControlStateSelected];\
+[button.titleLabel setFont:[UIFont systemFontOfSize:16]];\
+[button setTitleColor:[UIColor colorWithHexString:@"#FFFFFF"] forState:UIControlStateNormal];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.rightBarButtonItem = buttonItem;\
+}
+
+// 导航栏右按钮(只有文字)
+#define NAVIGATION_BAR_RIGHT_BUTTON_COLORTEXT(x, y, w, h, normal, selected, sel, color)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setTitle:normal forState:UIControlStateNormal];\
+[button setTitle:selected forState:UIControlStateSelected];\
+[button.titleLabel setFont:[UIFont systemFontOfSize:14]];\
+[button setTitleColor:[UIColor colorWithHexString:color] forState:UIControlStateNormal];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.rightBarButtonItem = buttonItem;\
+}
+
+
+// 导航栏右按钮
+#define NAVIGATION_BAR_RIGHT_BUTTON(x, y, w, h, normal, selected, sel)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setImage:[UIImage imageNamed:normal] forState:UIControlStateNormal];\
+[button setImage:[UIImage imageNamed:selected] forState:UIControlStateHighlighted];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.rightBarButtonItem = buttonItem;\
+}
+
+
+// 导航栏右按钮---图片加文字
+#define NAVIGATION_BAR_RIGHT_BUTTON_TITLE_AND_IMAGE(x, y, w, h, normal, selected, title ,sel)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setImage:[UIImage imageNamed:normal] forState:UIControlStateNormal];\
+[button setImage:[UIImage imageNamed:selected] forState:UIControlStateHighlighted];\
+[button setTitle:title forState:UIControlStateNormal];\
+[button.titleLabel setFont:[UIFont systemFontOfSize:15]];\
+button.contentEdgeInsets = UIEdgeInsetsMake(0,30, 0, 0);\
+button.imageEdgeInsets = UIEdgeInsetsMake(0,-10, 0, 0);\
+[button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];\
+[button setTitleColor:RGB(61, 105, 150) forState:UIControlStateHighlighted];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.rightBarButtonItem = buttonItem;\
+}
+
+
+
+
+// 导航栏左按钮，使用CustomView来自定义返回图标
+#define NAVIGATION_BAR_LEFT_BUTTON(x, y, w, h, normal, selected, sel)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setImage:[UIImage imageNamed:normal] forState:UIControlStateNormal];\
+[button setImage:[UIImage imageNamed:selected] forState:UIControlStateHighlighted];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.leftBarButtonItem = buttonItem;\
+}
+
+// 导航栏左按钮，自定义返回图标，这种可以通过修改leftBarButtonItem的tintColor来改变返回图标颜色
+#define EWTModifyNavigationBarLeftBarButtonItemWithImage(imageName, sel)\
+{\
+UIBarButtonItem *backBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:imageName] style:UIBarButtonItemStylePlain target:self action:sel];\
+self.navigationItem.leftBarButtonItem = backBarButtonItem;\
+}
+
+// 导航栏左按钮
+#define NAVIGATION_BAR_LEFT_BUTTON_ADJUST(x, y, w, h, normal, selected, sel)\
+{\
+UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+button.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 28);\
+[button setImage:[UIImage imageNamed:normal] forState:UIControlStateNormal];\
+[button setImage:[UIImage imageNamed:selected] forState:UIControlStateSelected];\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.leftBarButtonItem = buttonItem;\
+}
+
+// 导航栏左按钮(消息)
+#define NAVIGATION_BAR_LEFT_BUTTON_MSGCOUNT(x, y, w, h, normal, selected, sel)\
+{\
+MsgCountButton *button = [MsgCountButton buttonWithType:UIButtonTypeCustom];\
+button.frame = CGRectMake(x, y, w, h);\
+[button setImage:[UIImage imageNamed:normal] forState:UIControlStateNormal];\
+[button setImage:[UIImage imageNamed:selected] forState:UIControlStateSelected];\
+button.tag = 101;\
+[button addTarget:self action:@selector(sel) forControlEvents:UIControlEventTouchUpInside];\
+UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];\
+self.navigationItem.leftBarButtonItem = buttonItem;\
+}
+
+
 
 #endif /* YFLBase_h */
--- IOS/IOS/One/Controller/OneHomeViewController.m
+++ IOS/IOS/One/Controller/OneHomeViewController.m
@@ -8,32 +8,91 @@
 
 #import "OneHomeViewController.h"
 
-@interface OneHomeViewController ()
-
+@interface OneHomeViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *table;
 @end
 
 @implementation OneHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     self.title = @"ONE";
+    
+    [self initView];
+    
+    [self loadData];
+}
+
+-(void)initView{
+    [self.view addSubview:self.table];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"click" style:UIBarButtonItemStylePlain target:self action:@selector(rightClick:)];
+}
+
+-(void)loadData{
+    [self.table reloadData];
+}
+
+#pragma mark - UITableView Delegate And Datasource
+//UITableViewDelegate,UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identify = @"cellIdentify";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identify];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identify];
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma mark - 懒加载
+-(UITableView *)table{
+    if(!_table){
+        UITableView *table = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT-NAVIGATION_BAR_HEIGHT)];
+        _table = table;
+        _table.backgroundColor = RGB(242, 242, 242, 1.0);
+        //_table.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _table.delegate = self;
+        _table.dataSource = self;
+        [self.view addSubview:_table];
+        
+        //[_table registerClass:[CareerProfessionjingduCell class] forCellReuseIdentifier:@"CareerjindguCell"];
+    }
+    return _table;
+}
+
+
+#pragma mark - methord
+-(void)rightClick:(UIButton *)sender{
+    [self.table reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+    [self.table reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
